a1676c0c50f018dacd4713a7f555ff8567287d74
Vitaly Panyukhin <creatiwww@yandex.ru>
2022-04-11 02:07:29 +0300

    server

--- environments/playground/main.tf
+++ environments/playground/main.tf
@@ -7,10 +7,16 @@ module "vpc" {
 module "subnet_public" {
   source = "../../modules/subnets"
 
-  vpc_id            = module.vpc.aws_vpc_id
-  availability_zone = "us-east-1a"
-  cidr_block        = "10.0.0.0/24"
-  tag_name          = "Public-subnet"
+  vpc_id                  = module.vpc.aws_vpc_id
+  availability_zone       = "us-east-1a"
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  tag_name                = "Public-subnet"
+}
+
+resource "aws_route_table_association" "RT-IG-Association" {
+  subnet_id      = module.subnet_public.subnet_id
+  route_table_id = module.vpc.aws_route_table_id
 }
 
 module "jenkins_master" {
@@ -20,4 +26,5 @@ module "jenkins_master" {
   instance_type = "t3.micro"
   subnet_id     = module.subnet_public.subnet_id
   tag_name      = "Jenkins Master"
+  vpc_id        = module.vpc.aws_vpc_id
 }
